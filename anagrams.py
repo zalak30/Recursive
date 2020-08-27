711eb192a0884964e539549107d1072d4467f373
Zalak <zalakpatel534@gmail.com>
2020-08-27 18:58:22 +0530

    examples of recursion

---
+++ anagrams.py
@@ -0,0 +1,16 @@
+import fact
+
+
+def anagrams(s):
+    if s == "":
+        return [s]
+    else:
+        ans = []
+        for w in anagrams(s[1:]):
+            for pos in range(len(w)+1):
+                ans.append(w[:pos]+s[0]+w[pos:])
+    print(fact.fact(len(s)))
+    return ans
+
+
+print(anagrams("abc"))
